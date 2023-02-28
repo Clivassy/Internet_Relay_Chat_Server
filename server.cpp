c3f08b5f1ddf0e100406f88a1b81720d3337540a
Julia BATORO <jbatoro@e1r7p19.clusters.42paris.fr>
2023-02-28 12:01:40 +0100

    creating server

---
+++ server.cpp
@@ -0,0 +1,69 @@
+
+//-- Official libraries
+#include <iostream>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
+
+//-- Personnal Libraries 
+#include "colors.hpp"
+
+//-- Defines
+#define PORT 8080
+
+int main()
+{
+    //-- Server Process 
+
+    int serverFd;
+
+    //-- Socket()
+    //-- Creation du socket 
+    //  --> store the file descriptor returned by the socket : create a communication socket
+    //-- AF_INET : indicate use of IPV4 Protocole
+    //-- SOCK_STREAM : indicate use of TCP Protocole
+    //-- 0 : indicates default protocole should be used.
+
+    serverFd = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverFd < 0)
+    {
+        std::cout << BOLD_RED << "Error while creating socket" << RESET << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    std::cout << BOLD_YELLOW  << "Socket server created successfully" << RESET << std::endl;
+
+    //-- Enable to specify IP adress and Port Number to which the socket 
+    //-- must be linked to communicate.
+    struct sockaddr_in  serverAddr;
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY); // put automatic IP Adress
+   // std::cout << "IP adress du server a transmettre au client: " << htonl(INADDR_ANY) << std:: endl;
+
+    //-- Bind()
+    if ( bind(serverFd, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0)
+    {
+        std::cout << BOLD_RED << "Error while bindind socket to Port 8080" << RESET << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    std::cout << BOLD_YELLOW  << "Socket bind successfully to port 8080" << RESET << std::endl;
+    //-- Listen() --> wait for the client to approach the server to make a connection.
+    // int listen(int sockfd, int backlog);
+    // -- backlog == maximum 
+
+    //-- Accept() --> 
+
+    //-- read // wwrite : communication between processes
+
+    //-- CLOSE 
+
+    return (0);
+}
+
+
