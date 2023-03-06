ccce955bb73ffb8800314384390fd8a157031158
Julia BATORO <jbatoro@e1r7p21.clusters.42paris.fr>
2023-03-06 12:01:24 +0100

    update yann arzu

--- server.cpp
+++ server.cpp
@@ -10,16 +10,21 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <netdb.h>
+#include <poll.h>
+#include <vector>
 
 //-- Personnal Libraries 
 #include "colors.hpp"
 
 //-- Defines
-#define PORT 8080
+#define PORT 6697
+
+
 
 int main()
 {
     //-- Server Process 
+    //-- Vector to handle multiple connection to the server  
 
     int serverFd;
     int clientFd;
@@ -41,6 +46,12 @@ int main()
     }
     std::cout << BOLD_YELLOW  << "Socket server created successfully" << RESET << std::endl;
 
+    //-- Fill pollfd array 
+    std::vector<pollfd> pollfdArray;
+    pollfd server_pollfd = { serverFd, POLLIN, 0};
+    pollfdArray.push_back(server_pollfd);
+
+    //-- BIND
     //-- Enable to specify IP adress and Port Number to which the socket 
     //-- must be linked to communicate.
     struct sockaddr_in  serverAddr;
@@ -61,7 +72,8 @@ int main()
     //-- Listen() --> wait for the client to approach the server to make a connection.
     // int listen(int sockfd, int backlog);
     // -- backlog == maximum 
-    if (listen(serverFd, 5) < 0)
+    // SOMAXCONN defines the maximum number you're allowed to pass to listen().
+    if (listen(serverFd, SOMAXCONN) < 0)
     {
         std::cout << BOLD_RED << "Error while listening" << RESET << std::endl;
         exit(EXIT_FAILURE);    
@@ -76,17 +88,7 @@ int main()
         std::cout << BOLD_RED << "Error while accepting connection" << RESET << std::endl;
         exit(EXIT_FAILURE);     
     }
-
-    // 
-    while (serverFd > 0)
-    {
-        send(serverFd, buffer,bufferSize, 0 ); // alternative to the thread method
-        std::cout << BOLD_GREEN  << "Server connected" << RESET << std::endl;
-    }
-
-
-
-
+    std::cout << BOLD_GREEN  << "Server catch connection" << RESET << std::endl;
 
     //-- read // wwrite : communication between processes
     //-- CLOSE 
