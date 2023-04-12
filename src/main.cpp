da73cfc2d38ed2e0a4b3fb7b7f7697834bb2ec06
Julia BATORO <jbatoro@e1r7p21.clusters.42paris.fr>
2023-04-12 14:39:37 +0200

    main temp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "main.hpp"
+#include <signal.h>
 
 int main(int ac, char **av)
 {
@@ -7,5 +8,23 @@ int main(int ac, char **av)
 	server.init();
 	server.run();
 	server.terminate();
-
 }
+
+/*bool closeServer = false;
+
+void	handler(int signal)
+{
+	(void)signal;
+	closeServer = true;
+}
+
+int main(int ac, char **av)
+{
+	Server server;
+	parsing_arguments(ac, av, server);
+	signal(SIGINT, handler);
+	server.init();
+	while (closeServer == false)
+		server.run();
+	server.terminate();
+}*/
