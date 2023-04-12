8ddf9b9f1dfb7b3821dd897747c986055c8e739e
Arzu Guneslik <agunesli@student.42.fr>
2023-04-12 14:20:32 +0200

    continue to debug names cmd

--- src/10_tools/errors.hpp
+++ src/10_tools/errors.hpp
@@ -78,7 +78,10 @@
 
 # define ERR_TOOMANYCHANNELS(nick, channel) ("405 " + nick + " " + channel + " :You have joined too many channels\r\n")
 
-# define RPL_NAMREPLY(nick, user, host, channel) ("353 " + nick + "!" + user + "@" + host + " = " + channel + " :") // ajouter \r\n ou list des clients presents
+# define RPL_NAMREPLY(nick, user, host, channel) ("353 =" + nick + "!" + user + "@" + host + " = " + channel + " :\r\n") // ajouter \r\n ou list des clients presents
+
+
+# define RPL_NAMREPLY2(nick, channel) ("353 =" + channel + ":" + nick + "\r\n")
 //2e message standard recu quand on rejoint un channel
 
 # define RPL_ENDOFNAMES(nick, user, host, channel) ("366 " + nick + "!" + user + "@" + host + " " + channel + " :End of NAMES list\r\n")
--- src/20_class/client/cmdNAMES.cpp
+++ src/20_class/client/cmdNAMES.cpp
@@ -4,25 +4,29 @@ bool	Client::cmdNAMES(std::vector<std::string> &cmd)
 {
 	if (this->status != CONNECTED)
 		return (false);
-	this->server.channelList	
+//	this->server.channelList
+	std::vector<std::string> listOfChannelToShow = split(cmd[0], ",");
+	if (cmd.size() != 1)
+		std::vector<std::string> listOfChannelToShow = split(cmd[1], ",");
 
-	std::vector<std::string> listOfChannelToShow = split(cmd[1], ",");
 	std::string channel_name;
 
 	for (std::vector<std::string>::iterator it = listOfChannelToShow.begin(); it != listOfChannelToShow.end(); it++)
 	{
 		channel_name = *it;
 		toLowerStr(channel_name);
-		for (std::map<std::string, Channel>::iterator it=this->channelList.begin(); it != this->channelList.end(); it++)
+		for (std::map<std::string, Channel>::iterator it_channel=this->server.channelList.begin(); it_channel != this->server.channelList.end(); it_channel++)
 		{
-			if (channel_name == it->first)
+			std::cout << "channel_names " << channel_name << std::endl;
+			if (channel_name == "names" || channel_name == it_channel->first)
 			{
-				for (std::set<std::string>::iterator it_client=it->second.clientConnected.begin(); it_client != it->second.clientConnected.end(); it_client++)
+		//		this->sendMessage(it_channel->first + "\n");
+				for (std::set<std::string>::iterator it_client=it_channel->second.clientConnected.begin(); it_client != it_channel->second.clientConnected.end(); it_client++)
 				{
-					std::cout << *it_client;
-					if(++it_client != it->second.clientConnected.end())
-					std::cout << ", ";
-				--it_client;
+		//			this->sendMessage("Bouhhh\n");
+		//			std::vector<Client>::iterator cl = this->server.getClient(*it_client);
+		//			this->sendMessage(RPL_NAMREPLY(cl->userInfos.nickName, cl->userInfos.userName, cl->userInfos.hostName, channel_name));
+					this->sendMessage(RPL_NAMREPLY2(*it_client, it_channel->first));
 				}
 			}
 		}
